51cfd5050a26222f0a1d1c715e90175397497f7e
Surafel Melliyon <surafelmelliyon111@gmail.com>
2026-06-28 20:42:54 +0300

    fix  the axios

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
-
+   console.log(import.meta.env.VITE_API_URL)
 // Attach JWT token to every request
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
